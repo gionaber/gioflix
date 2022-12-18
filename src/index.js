d285fe3e0d90d05107c5c203ec5ecc0180596e16
anıl kankavi <kankavi@anl-MacBook-Air.local>
2022-12-18 22:11:26 +0300

    file and folder structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+/* eslint-disable comma-dangle */
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './components/App'
-ReactDOM.render(<App />, document.getElementById('root'))
- 
+import App from './components/App';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
